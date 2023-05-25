2335335f7878b6685b666cd891a0c018d7ec27d6
Firewall-Shodan <30272561+Firewall-Shodan@users.noreply.github.com>
2023-05-25 02:52:51 +0200

    Script Para Inserir dados de Oracle para Mongo DB
    Este script visa fazer o fetch de dados do oracle para inserir numa coleção em mongo DB

---
+++ Database SQL/Script Python/query_from_sql.py
@@ -0,0 +1,53 @@
+import cx_Oracle
+from pymongo import MongoClient
+
+# Conectar a  Oracle database
+connect = cx_Oracle.connect('store/marcos2020@localhost:1521/xe')
+
+# Query e fetch dos dados
+oracle_cursor = connect.cursor()
+oracle_cursor.execute('SELECT p.product_id, p.product_name, c.category_id, c.category_name, p.sku, p.price, p.discount_id, p.created_at, p.last_modified, s.quantity, s.max_stock_quantity, s.unit FROM product p JOIN stock s ON p.product_id = s.product_id JOIN product_categories c ON p.category_id = c.category_id')
+oracle_data = oracle_cursor.fetchall()
+
+# Transformar em dicionário para o MongoDB
+products = []
+for row in oracle_data:
+    product = {
+        'product_id': row[0],
+        'product_name': row[1],
+        'category': {
+            'category_id': row[2],
+            'category_name': row[3]
+        },
+        'sku': row[4],
+        'price': row[5],
+        'discount_id': row[6],
+        'created_at': row[7],
+        'last_modified': row[8],
+        'stock': {
+            'quantity': row[9],
+            'max_stock_quantity': row[10],
+            'unit': row[11]
+        }
+
+    }
+    products.append(product)
+
+# fechar a conexão
+oracle_cursor.close()
+connect.close()
+
+
+# Connect to MongoDB Atlas
+client = MongoClient('mongodb+srv://marcos:M##rcos91@tpratico.9i2ffuf.mongodb.net/?retryWrites=true&w=majority')
+
+# Access the desired database
+db = client['Tpratico']
+
+
+# Inserir dados no MongoDB
+product_collection = db['produtos']
+product_collection.insert_many(products)
+
+# fechar a conexão
+client.close()
